7ad167f82d3fd629ff5db031802c384d6a739912
kevinsslin <kk123750964@gmail.com>
2024-03-23 18:19:25 +0800

    Finish draw settlement api

--- server/app/api/actions.tsx
+++ server/app/api/actions.tsx
@@ -12,10 +12,32 @@ export async function saveDraw(draw: Draw) {
   console.log(`${getDateTag()} Draw ${draw.id} saved successfully`)
 }
 
-export async function updateDrawStatus(drawId: string) {
+export async function getDrawById(drawId: string): Promise<Draw | null> {
+  try {
+    const drawData = await kv.hgetall(`draw:${drawId}`)
+    if (!drawData || Object.keys(drawData).length === 0) {
+      console.error(`Draw with ID ${drawId} not found.`)
+      return null
+    }
+    return drawData as Draw
+  } catch (error) {
+    console.error(`Error retrieving draw with ID ${drawId}:`, error)
+    return null
+  }
+}
+
+export async function closeDraw(drawId: string) {
   const status = await kv.hget(`draw:${drawId}`, 'status')
   if (status === 0) {
     await kv.hincrby(`draw:${drawId}`, 'status', 1)
     console.log(`${getDateTag()} Draw ${drawId} status updated to closed`)
+  } else {
+    console.error(`Draw ${drawId} already closed`)
   }
+}
+
+export async function deleteDrawById(drawId: string) {
+  await kv.del(`draw:${drawId}`)
+  await kv.zrem('polls_by_date', drawId)
+  console.log(`${getDateTag()} Draw ${drawId} deleted successfully`)
 }
